6e7894527633eb7a6a14fd19c0455b6441b582e8
AlphaDevelopment <109308073+bylickilabs@users.noreply.github.com>
2024-06-17 08:24:26 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+<script language="JavaScript1.2">
+function disableselect(e){
+return false
+}
+function reEnable(){
+return true
+}
+document.onselectstart=new Function ("return false")
+if (window.sidebar){
+document.onmousedown=disableselect
+document.onclick=reEnable
+}
+</script>
+
+</head>
+<body oncontextmenu="return false">
+<script language="JavaScript1.2">
+function disableselect(e){
+return false
+}
+function reEnable(){
+return true
+}
+document.onselectstart=new Function ("return false")
+if (window.sidebar){
+document.onmousedown=disableselect
+document.onclick=reEnable
+}
+</script>
